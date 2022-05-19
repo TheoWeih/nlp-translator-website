8612d8ad61af5b76260169514f8f1da4c5523229
Theo Weih <theoweihprachtitzky@web.de>
2022-05-18 21:55:01 -0700

    Added API ping

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/api/ping')
+def ping():
+    return "ping"
 
 
 if __name__ == "__main__":
